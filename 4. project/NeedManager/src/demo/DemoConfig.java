3ba8fe4d927e6bc4fc1b21b416b44a68d0046402
guangwei-home <862303645@qq.com>
2016-09-03 19:40:27 +0800

    hello world ,jfinal

---
+++ 4. project/NeedManager/src/demo/DemoConfig.java
@@ -0,0 +1,40 @@
+package demo;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+
+public class DemoConfig extends JFinalConfig {
+
+	@Override
+	public void configConstant(Constants me) {
+		me.setDevMode(true);
+	}
+
+	@Override
+	public void configRoute(Routes me) {
+		me.add("/hello", HelloController.class);
+	}
+
+	@Override
+	public void configPlugin(Plugins me) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void configInterceptor(Interceptors me) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void configHandler(Handlers me) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
